50ed8970328a3cff21f20fe8660b1f1bd5dcbbfb
Balachandar <84532358+BalachandarArun@users.noreply.github.com>
2022-09-18 15:59:44 +0530

    API SESSION

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" title="hello title"/>
+  
+  <CustomComp/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import CustomComp from './components/CustomComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    CustomComp
   }
 }
 </script>
